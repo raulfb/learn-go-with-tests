0a0f6b5c8227b0360ad20d56aa8fbeece1bc304a
raulfb <43234547+raulfb@users.noreply.github.com>
2026-03-03 16:14:37 +0100

    sumAll

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,16 +1,29 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 6
 
-	got := Sum(numbers)
-	want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{2, 3}, []int{1, 3})
+	want := []int{5, 4}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
-
 }
