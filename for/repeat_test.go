ccb6e5a90e359e1b9139c3a09737b3b32c815351
raulfb <43234547+raulfb@users.noreply.github.com>
2026-03-03 10:57:18 +0100

    test fallando

--- for/repeat_test.go
+++ for/repeat_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
